669b5b8176d6091231788d8a51d303e7ccd75bb9
Christopher Wood <cjwood032@gmail.com>
2018-08-05 17:33:39 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,10 +9,13 @@ class StudentsController < ActionController::Base
   end
 
   def update
+    @student= Student.find(params[:id])
+    student.uptade(params[:student])
+    redirect_to student
   end
 
   def show
     @student= Student.find(params[:id])
   end
-  
+
 end
